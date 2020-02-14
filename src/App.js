c70ed04cb825fb08434c1616c1b93c6ac02a1e5b
Manoj S M <manojsm293@gmail.com>
2020-02-14 18:15:09 +0530

    Initialized a new react application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// imports
+import React from 'react';
+
+// local imports
+
+const App = () => (
+    <div>
+        This is the App js
+    </div>
+)
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// local imports
+import App from './App';
+
+ReactDOM.render(<App /> , document.querySelector('#root'));
